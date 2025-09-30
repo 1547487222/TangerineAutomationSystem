ea0f7fd14911854ee4b40c95c305f8340a754ae2
SugarColaM <57125126+1547487222@users.noreply.github.com>
2025-10-01 05:19:26 +0800

    feat(vscode-ui): add process flow editor models, viewmodel, view and platform grpc client

--- Models/FlowNode.cs
+++ Models/FlowNode.cs
@@ -1,7 +1,33 @@
-// Content for FlowNode.cs
+using System;
+using System.ComponentModel;
 
-public class FlowNode
+namespace TangerineAutomationSystem.Models
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
+    public enum FlowNodeKind
+    {
+        PlatformTask,
+        ModuleAction,
+        Transfer,
+        Resource
+    }
+
+    public class FlowNode : INotifyPropertyChanged
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        private string _name = "Node";
+        public string Name { get => _name; set { _name = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name))); } }
+
+        public FlowNodeKind Kind { get; set; } = FlowNodeKind.ModuleAction;
+        public string? ModuleRef { get; set; }
+        public string? PlatformTaskRef { get; set; }
+        public string? ResourceRef { get; set; }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public object Config { get; set; } = new { };
+        public string ConfigJson { get; set; } = "{}";
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
 }
